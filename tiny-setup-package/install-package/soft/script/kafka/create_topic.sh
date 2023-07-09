fae9ec92aa52d872a25fca93522326fb83b8165a
xtpeach <xtpeach@outlook.com>
2023-07-09 23:54:28 +0800

    update:kafka

--- tiny-setup-package/install-package/soft/script/kafka/create_topic.sh
+++ tiny-setup-package/install-package/soft/script/kafka/create_topic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bash kafka-topics.sh --bootstrap-server kafka128:9092 --list
-bash kafka-topics.sh --bootstrap-server kafka128:9092 --topic first_topic --create --partitions 1 --replication-factor 3
+bash kafka-topics.sh --bootstrap-server kafka128:9092 --topic first_topic --create --partitions 2 --replication-factor 3
 bash kafka-topics.sh --bootstrap-server kafka128:9092 --topic first_topic --describe
-bash kafka-topics.sh --bootstrap-server kafka128:9092 --topic first_topic --alter --partitions 2
+bash kafka-topics.sh --bootstrap-server kafka128:9092 --topic first_topic --alter --partitions 3 --replication-factor 3
+bash kafka-topics.sh --bootstrap-server kafka128:9092 --topic first_topic --alter --replication-factor 3
