62d455c2a189032eea02f14ff19b4c0aaca0be4b
xtpeach <xtpeach@outlook.com>
2023-09-06 22:47:20 +0800

    update:监控程序模板

---
+++ tiny-setup-package/install-package/monitor/clickhouse/do_check_clickhouse.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# 监控间隔时间
+sleep_seconds=5
+
+# 环境变量
+source /etc/profile
+
+# 日志文件路径
+log_dir=/data/logs/monitor/
+# 日志文件
+log_file=$log_dir/do_check_clickhouse.log
+# 创建日志文件路径
+mkdir -p $log_dir
+# 创建日志文件
+touch $log_file
+
+# 检查
+do_check_clickhouse() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') check clickhouse step1 " >> $log_file
+}
+
+# 循环执行检查
+while (true); do
+    # 执行时间间隔
+    sleep $sleep_seconds
+    echo "$(date '+%Y-%m-%d %H:%M:%S') check clickhouse ..." >> $log_file
+
+    # 执行检查步骤
+    do_check_clickhouse
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') check clickhouse done" >> $log_file
+    echo "..." >> $log_file
+done
---
+++ tiny-setup-package/install-package/monitor/job/do_check_job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# 监控间隔时间
+sleep_seconds=5
+
+# 环境变量
+source /etc/profile
+
+# 日志文件路径
+log_dir=/data/logs/monitor/
+# 日志文件
+log_file=$log_dir/do_check_job.log
+# 创建日志文件路径
+mkdir -p $log_dir
+# 创建日志文件
+touch $log_file
+
+# 检查
+do_check_job() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') check job step1 " >> $log_file
+}
+
+# 循环执行检查
+while (true); do
+    # 执行时间间隔
+    sleep $sleep_seconds
+    echo "$(date '+%Y-%m-%d %H:%M:%S') check job ..." >> $log_file
+
+    # 执行检查步骤
+    do_check_job
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') check job done" >> $log_file
+    echo "..." >> $log_file
+done
