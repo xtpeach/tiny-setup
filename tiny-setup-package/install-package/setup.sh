0cde352c46ee8c5f76f96126bd268bb3f820c6cc
xtpeach <xtpeach@outlook.com>
2023-10-08 23:06:52 +0800

    update

--- tiny-setup-package/install-package/setup.sh
+++ tiny-setup-package/install-package/setup.sh
@@ -13,7 +13,7 @@ source ./soft/script/common.sh
 
 # 2. 执行安装逻辑
 # 进度条
-sleep_time=2s
+sleep_time=5s
 # 刷新 testProgressBar.log 放入 “start” 字符串到首行
 log_info "start setup"
 {
@@ -23,22 +23,30 @@ log_info "start setup"
     cd $INSTALL_PACKAGE_DIR/soft/script
     bash install.sh
 
-  # 将安装日志输入到日志文件中
+    # 将安装日志输入到日志文件中
   } >>$LOG_FILE 2>&1 &
 
   # 进度条显示
-  for ((i = 0; i <= 99; i += 10)); do
+  bar_i=0
+  while [[ 1 == 1 ]]; do
     sleep $sleep_time
     # 判断执行日志最后一行是否打印的成功标志“#@success@#”
     lastLine=$(sed -n '$p' $LOG_FILE)
-    if [[ "$lastLine"x = "#@success@#"x ]]; then
+    if [[ "$lastLine"x == "#@success@#"x ]]; then
       # 若已成功快速达到100
       sleep_time=0.5
       # 若已成功直接进度到达100
-      #i=100
+      bar_i=100
+      break
     fi
-    echo $i
+    echo $bar_i
+    if [[ $bar_i -lt 99 ]]; then
+      ((bar_i++))
+    fi
   done
 
   # 展示进度条
-} | whiptail --gauge "Please wait while installing" 6 60 0
+} | whiptail --gauge "Please wait while installing ..." 6 60 0
+
+# 提示安装完毕
+whiptail --title "Setup completed" --msgbox " Choose Ok to continue." 10 60
--- tiny-setup-package/install-package/soft/script/ini_operator.sh
+++ tiny-setup-package/install-package/soft/script/ini_operator.sh
@@ -19,10 +19,10 @@ value=$5
 msg="Please input the param <get|set> <file> <section> <key> [value]"
 
 # 定制化shell输出
-function custom_print(){
-    echo -e "\033[5;34m ***** \033[0m"
-    echo -e "\033[32m $@ ! \033[0m"
-    echo -e "\033[5;34m ***** \033[0m"
+function custom_print() {
+  echo -e "\033[5;34m ***** \033[0m"
+  echo -e "\033[32m $@ ! \033[0m"
+  echo -e "\033[5;34m ***** \033[0m"
 }
 
 # 获取配置文件指定section指定key的value
@@ -30,29 +30,65 @@ function get_opt() {
     file=$1
     section=$2
     key=$3
-    val=$(awk -F "$delimeter" '/\['${section}'\]/{a=1}a==1&&$1~/'${key}'/{print $2;exit}' $file)
-    echo ${val}
+
+    # 设置IFS为等号，以便按照key=value对进行分割
+    IFS="="
+
+    # 读取文件内容，并逐行处理
+    while read -r line; do
+        # 如果行以#开头，则跳过
+        if [[ $line =~ ^# ]]; then
+            continue
+        fi
+
+        # 如果行以[section]开头，则设置flag为1，表示开始读取指定section的内容
+        if [[ $line =~ ^\[$section\] ]]; then
+            flag=1
+            continue
+        fi
+
+        # 如果flag为1，表示已经进入指定section的内容区域
+        if [[ $flag -eq 1 ]]; then
+            # 如果行为空行，则表示已经读取完指定section的内容，退出循环
+            if [[ -z $line ]]; then
+                break
+            fi
+
+            # 将行按照等号分割为key和value
+            read -r k v <<< "$line"
+
+            # 去除key和value的前后空格
+            k=$(echo "$k" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+            v=$(echo "$v" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+
+            # 如果key等于指定的key，则输出对应的value并退出循环
+            if [[ $k == $key ]]; then
+                echo "$v"
+                break
+            fi
+        fi
+    done < "$file"
 }
 
 # 更新配置文件指定section指定key的value
 function set_opt() {
-    file=$1
-    section=$2
-    key=$3
-    val=$4
-    awk -F "$delimeter" '/\['${section}'\]/{a=1} (a==1 && "'${key}'"==$1){gsub($2,"'${val}'");a=0} {print $0}' ${file} 1<>${file}
+  file=$1
+  section=$2
+  key=$3
+  val=$4
+  awk -F "$delimeter" '/\['${section}'\]/{a=1} (a==1 && "'${key}'"==$1){gsub($2,"'${val}'");a=0} {print $0}' ${file} 1<>${file}
 }
 
 # 判断输入参数
-if [[ -z $operate || $operate = "help" || $operate = "-h" || -z $section || -z $key ]]; then
-    custom_print $msg
-elif [[ $operate = "get" ]]; then
-    val=$(get_opt $file $section $key)
-    echo $val
-elif [[ $operate = "set" && $value ]]; then
-    set_opt $file $section $key $value
-    msg='update success'
-    custom_print $msg
+if [[ -z $operate || $operate == "help" || $operate == "-h" || -z $section || -z $key ]]; then
+  custom_print $msg
+elif [[ $operate == "get" ]]; then
+  val=$(get_opt $file $section $key)
+  echo $val
+elif [[ $operate == "set" && $value ]]; then
+  set_opt $file $section $key $value
+  msg='update success'
+  custom_print $msg
 else
-    custom_print $msg
+  custom_print $msg
 fi
