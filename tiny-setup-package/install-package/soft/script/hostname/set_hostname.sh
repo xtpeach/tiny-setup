efec06d64d6935285e537be37225c5a85efa0105
xutao4 <xutao4@sungrowpower.com>
2024-02-21 13:39:41 +0800

    feature(update): 小修改

--- tiny-setup-package/install-package/soft/script/hostname/set_hostname.sh
+++ tiny-setup-package/install-package/soft/script/hostname/set_hostname.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# 接受一个字符串参数，用于设置主机实例的 hostname
 host_name_str=$1
 
 # 输入名称不为空
 [[ "$host_name_str"x != x ]] && {
+  # 设置主机的 hostname
   hostnamectl set-hostname $host_name_str
+  # 查看主机的 hostname
   hostnamectl status
+  # 检查 hostname 是否设置成功
   set_hostname_flag=$(cat /etc/hosts | grep -v 'grep' | grep $host_name_str)
+  # 如果检查到 hostname 设置成功了则添加 hosts 配置
   if [ "$set_hostname_flag"x == "x" ]; then
+    # 添加 hosts 配置到 /etc/hosts 文件当中
     echo "127.0.0.1   $(hostname)" >>/etc/hosts
   fi
 }
 
 # 输入名称为空
 [[ "$host_name_str"x == x ]] && {
-  echo "null"
+  # 打印输出，hostname 输入为空
+  echo "set_hostname input hostname is null"
 }
--- tiny-setup-package/install-package/soft/script/open_port.sh
+++ tiny-setup-package/install-package/soft/script/open_port.sh
@@ -2,12 +2,19 @@
 # source
 source ./common.sh
 
-# "centos"
+# centos
 if [[ "$OS_NAME" == "centos" ]]; then
+  # 循环开启所有配置要开启的端口
   for index in "${!OPEN_PORT_ARRAY[@]}"; do
+    # 打印日志，记录开启的端口
     log_note "firewall-cmd --zone=public --add-port=${OPEN_PORT_ARRAY[$index]}/tcp --permanent"
+    # 开启端口
     firewall-cmd --zone=public --add-port=${OPEN_PORT_ARRAY[$index]}/tcp --permanent
   done
+
+  # 展示所有已经开启的端口
   firewall-cmd --list-ports
+
+  # 防火墙重新加载所有端口
   firewall-cmd --reload
 fi
