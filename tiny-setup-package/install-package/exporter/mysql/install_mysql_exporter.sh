a453bc83d1fc53c1f0de426a004e88351956d939
xutao4 <xutao4@sungrowpower.com>
2024-03-15 16:28:00 +0800

    feature(exporter): 添加exporter

--- tiny-setup-package/install-package/exporter/mysql/install_mysql_exporter.sh
+++ tiny-setup-package/install-package/exporter/mysql/install_mysql_exporter.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# exporter port
+exporter_port=9104
+
 # mysql 的用户名/密码
-mysql_username=root
+mysql_username="root"
 mysql_password=123456
+mysql_host="localhost"
+mysql_port=3306
 
 # mysql exporter 的版本/下载地址
-mysql_exporter_version=mysqld_exporter-0.12.1.linux-amd64.tar.gz
+mysql_exporter_version=mysqld_exporter-0.12.1.linux-amd64
+mysql_exporter_version_file=mysqld_exporter-0.12.1.linux-amd64.tar.gz
 mysql_exporter_version_url=https://github.com/prometheus/mysqld_exporter/releases/download/v0.12.1/mysqld_exporter-0.12.1.linux-amd64.tar.gz
 
 base_path=$(dirname "${BASH_SOURCE[0]}")
@@ -14,20 +20,20 @@ base_path=$(dirname "${BASH_SOURCE[0]}")
 mkdir -p /home/exporter/mysql
 
 # 判断当前路径下面有没有 mysql exporter 安装文件，如果有就将其拷贝到安装目录
-if [[ ! -f $mysql_exporter_version ]]; then
-  cp -a $base_path/$mysql_exporter_version /home/exporter/mysql/
+if [[ ! -f $mysql_exporter_version_file ]]; then
+  cp -a $base_path/$mysql_exporter_version_file /home/exporter/mysql/
 fi
 
 # 进入 mysql exporter 安装目录
 cd /home/exporter/mysql
 
 # 下载 mysql exporter 安装文件
-if [[ ! -f $mysql_exporter_version ]]; then
+if [[ ! -f $mysql_exporter_version_file ]]; then
   wget $mysql_exporter_version_url
 fi
 
 # 解压安装文件
-tar zxvf mysqld_exporter-0.12.1.linux-amd64.tar.gz  -C /usr/local/
+tar zxvf $mysql_exporter_version_file -C /usr/local/
 
 # 进入 /usr/local/
 cd /usr/local/
@@ -50,8 +56,8 @@ cat >/usr/lib/systemd/system/mysqld_exporter.service  <<EOF
 [Unit]
 Description=Prometheus
 [Service]
-Environment=DATA_SOURCE_NAME=$mysql_username:$mysql_password@(localhost:20002)/
-ExecStart=/usr/local/mysqld_exporter/mysqld_exporter --config.my-cnf=/usr/local/mysqld_exporter/.my.cnf --web.listen-address=:9104
+Environment=DATA_SOURCE_NAME=$mysql_username:$mysql_password@($mysql_host:$mysql_port)/
+ExecStart=/usr/local/mysqld_exporter/mysqld_exporter --config.my-cnf=/usr/local/mysqld_exporter/.my.cnf --web.listen-address=:$exporter_port
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target
---
+++ tiny-setup-package/install-package/exporter/redis/install_redis_exporter.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# exporter port
+exporter_port=9121
+
+# redis 的用户名/密码
+redis_password=123456
+redis_host="localhost"
+redis_port=6379
+
+# redis exporter 的版本/下载地址
+redis_exporter_version=redis_exporter-v1.29.0.linux-amd64
+redis_exporter_version_file=redis_exporter-v1.29.0.linux-amd64.tar.gz
+redis_exporter_version_url=https://github.com/oliver006/redis_exporter/releases/download/v1.29.0/redis_exporter-v1.29.0.linux-amd64.tar.gz
+
+
+base_path=$(dirname "${BASH_SOURCE[0]}")
+
+# 创建 /home/exporter/redis 目录
+mkdir -p /home/exporter/redis
+
+# 判断当前路径下面有没有 redis exporter 安装文件，如果有就将其拷贝到安装目录
+if [[ ! -f $redis_exporter_version_file ]]; then
+  cp -a $base_path/$redis_exporter_version_file /home/exporter/redis/
+fi
+
+# 进入 redis exporter 安装目录
+cd /home/exporter/redis
+
+# 下载 redis exporter 安装文件
+if [[ ! -f $redis_exporter_version_file ]]; then
+  wget $redis_exporter_version_url
+fi
+
+# 解压安装文件
+tar zxvf $redis_exporter_version_file  -C /usr/local/
+
+# 进入 /usr/local/
+cd /usr/local/
+
+# 添加超链接
+ln -s $redis_exporter_version/ redis_exporter
+
+# 创建 service 文件
+cat >/usr/lib/systemd/system/redis_exporter.service  <<EOF
+[Unit]
+Description=Prometheus
+[Service]
+ExecStart=/usr/local/redis_exporter/redis_exporter -redis.addr $redis_host:$redis_port -redis.password $redis_password --web.listen-address=:$exporter_port
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 对 systemd 服务或单元文件进行更改后，需要运行 systemctl daemon-reload 命令来通知 systemd 守护程序重新加载配置
+systemctl daemon-reload
+
+# 启用 redis exporter
+systemctl enable redis_exporter
+
+# 重启 redis exporter service
+systemctl restart redis_exporter
+
+# 查看 redis exporter 状态
+systemctl status redis_exporter
+
+# 下面开通防火墙端口需要根据不同的操作系统来
+# 开启防火墙端口(CentOS)
+firewall-cmd --zone=public --add-port=$exporter_port/tcp --permanent
+firewall-cmd --reload
