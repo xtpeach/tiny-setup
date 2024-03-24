b5909c7717909047fc53bd90fb3123712fdd6b2f
xtpeach <xtpeach@outlook.com>
2024-03-24 23:55:32 +0800

    添加webssh安装及镜像

--- tiny-setup-package/install-package/monitor/webssh/install_webssh.sh
+++ tiny-setup-package/install-package/monitor/webssh/install_webssh.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# ssh 可以直接通过链接连上服务器
+# 举例：http://192.168.96.128:8888/?hostname=192.168.96.129&username=root&password=MTIzNDU2
+
 # 解压源码包
 tar -zxvf webssh-1.6.2.tar.gz
+
+# 加载镜像
+docker load < webssh-162_web.tar
 
 # 进入源码包
 cd webssh-1.6.2
