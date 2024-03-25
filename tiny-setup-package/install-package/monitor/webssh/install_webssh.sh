c69bbd44be6e604f87a982aef4099b97da54bb9e
xutao4 <xutao4@sungrowpower.com>
2024-03-25 09:00:50 +0800

    feature(exporter): 添加举例的端口号

--- tiny-setup-package/install-package/monitor/webssh/install_webssh.sh
+++ tiny-setup-package/install-package/monitor/webssh/install_webssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ssh 可以直接通过链接连上服务器
-# 举例：http://192.168.96.128:8888/?hostname=192.168.96.129&username=root&password=MTIzNDU2
+# 举例：http://192.168.96.128:8888/?hostname=192.168.96.129&port=22&username=root&password=MTIzNDU2
 
 # 解压源码包
 tar -zxvf webssh-1.6.2.tar.gz
