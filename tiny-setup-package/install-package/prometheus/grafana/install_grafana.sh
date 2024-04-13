9ebca2762d3a656e8449160a3437e2f543176732
xutao4 <xutao4@sungrowpower.com>
2024-04-13 10:14:03 +0800

    feature(prometheus): 修改docker-compose文件命名以及安装时候的端口开放

--- tiny-setup-package/install-package/prometheus/grafana/install_grafana.sh
+++ tiny-setup-package/install-package/prometheus/grafana/install_grafana.sh
@@ -12,3 +12,15 @@ mkdir -p ./data
 chmod -R 777 ./data
 # 启动容器
 docker-compose up -d
+
+# grafana 端口
+grafana_port=3000
+
+# 下面开通防火墙端口需要根据不同的操作系统来
+# 开启防火墙端口(CentOS)
+if [[ $(systemctl is-active --quiet firewalld) ]]; then
+  firewall-cmd --zone=public --add-port=$grafana_port/tcp --permanent
+  firewall-cmd --reload
+else
+  iptables -I INPUT -p tcp --dport $grafana_port -j ACCEPT
+fi
--- tiny-setup-package/install-package/prometheus/prometheus/install_prometheus.sh
+++ tiny-setup-package/install-package/prometheus/prometheus/install_prometheus.sh
@@ -9,3 +9,15 @@ mkdir -p ./data
 chmod -R 777 ./data
 # 启动容器
 docker-compose up -d
+
+# prometheus 端口
+prometheus_port=9090
+
+# 下面开通防火墙端口需要根据不同的操作系统来
+# 开启防火墙端口(CentOS)
+if [[ $(systemctl is-active --quiet firewalld) ]]; then
+  firewall-cmd --zone=public --add-port=$prometheus_port/tcp --permanent
+  firewall-cmd --reload
+else
+  iptables -I INPUT -p tcp --dport $prometheus_port -j ACCEPT
+fi
