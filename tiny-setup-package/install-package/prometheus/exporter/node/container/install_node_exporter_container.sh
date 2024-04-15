1ed562f0bb8be57ab2f57eb113ea06ad11a1b932
xutao4 <xutao4@sungrowpower.com>
2024-04-15 15:23:27 +0800

    feature(node_exporter):添加主机监控容器安装

--- tiny-setup-package/install-package/prometheus/exporter/node/container/install_node_exporter_container.sh
+++ tiny-setup-package/install-package/prometheus/exporter/node/container/install_node_exporter_container.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# 当前路径
+base_path=$(dirname "${BASH_SOURCE[0]}")
+
+# 判断镜像文件是否存在，如果存在就加载一下
+if [[ -f node-exporter.tar ]]; then
+  cp -a $base_path/node-exporter.tar /home/exporter/node/
+  docker load < $base_path/node-exporter.tar
+fi
 
 # 安装 exporter 容器
 docker-compose up -d
