69ad7ad14382c359ed212c52fb226919f5943ae4
xutao4 <xutao4@sungrowpower.com>
2024-04-15 15:34:41 +0800

    feature(mysql_exporter):添加mysql监控容器安装

--- tiny-setup-package/install-package/prometheus/exporter/mysql/container/install_mysql_exporter_container.sh
+++ tiny-setup-package/install-package/prometheus/exporter/mysql/container/install_mysql_exporter_container.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# 当前路径
+base_path=$(dirname "${BASH_SOURCE[0]}")
+
+# 判断镜像文件是否存在，如果存在就加载一下
+if [[ -f mysqld-exporter.tar ]]; then
+  docker load < $base_path/mysqld-exporter.tar
+fi
+
+# 进入 /usr/local/mysqld_exporter
+mkdir -p /usr/local/mysqld_exporter
+cd /usr/local/mysqld_exporter
+
+# 创建 .my.cnf 该文件为隐藏文件
+cat > .my.cnf <<EOF
+[client]
+user=$mysql_username
+password=$mysql_password
+EOF
+
 # 安装 exporter 容器
 docker-compose up -d
--- tiny-setup-package/install-package/prometheus/exporter/node/container/install_node_exporter_container.sh
+++ tiny-setup-package/install-package/prometheus/exporter/node/container/install_node_exporter_container.sh
@@ -2,11 +2,22 @@
 # 当前路径
 base_path=$(dirname "${BASH_SOURCE[0]}")
 
+# exporter port
+exporter_port=9100
+
 # 判断镜像文件是否存在，如果存在就加载一下
 if [[ -f node-exporter.tar ]]; then
-  cp -a $base_path/node-exporter.tar /home/exporter/node/
   docker load < $base_path/node-exporter.tar
 fi
 
 # 安装 exporter 容器
 docker-compose up -d
+
+# 下面开通防火墙端口需要根据不同的操作系统来
+# 开启防火墙端口(CentOS)
+if [[ $(systemctl is-active --quiet firewalld) ]]; then
+  firewall-cmd --zone=public --add-port=$exporter_port/tcp --permanent
+  firewall-cmd --reload
+else
+  iptables -I INPUT -p tcp --dport $exporter_port -j ACCEPT
+fi
