0ce823e25941894d753ac29bcadfbe9a10ec22e7
xutao4 <xutao4@sungrowpower.com>
2024-04-20 14:50:13 +0800

    小修改

--- tiny-setup-package/install-package/soft/script/base64/base64encode.sh
+++ tiny-setup-package/install-package/soft/script/base64/base64encode.sh
@@ -6,4 +6,5 @@ read -p "Enter the string to encode: " input_string
 # 对输入字符串进行base64编码
 encoded_string=$(echo -n $input_string | base64)
 
+# 将 base64 加密之后的字符串打印出来
 echo "Encoded string: $encoded_string"
