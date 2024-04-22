c39210516ea28a640c4c6b04f27309b0de25ced6
xtpeach <xtpeach@outlook.com>
2024-04-23 01:48:20 +0800

    小修改

--- tiny-setup-package/install-package/setup.sh
+++ tiny-setup-package/install-package/setup.sh
@@ -6,6 +6,53 @@
 # bash脚本这一层不要做得复杂，越简单约好！
 # 尽量让每个bash脚本都能独立运行，减少相互依赖，方便运维
 
+# --------------------------------------------------------------------
+{
+  # install package dir
+  # 上传到 /opt/ 目录下面，然后安装的时候拷贝到 /home/ 下面
+  # -- *** [/opt/tiny-setup-package/install-package] *** --
+  INSTALL_PACKAGE_DIR=/opt/tiny-setup-package/install-package
+
+  # install dir
+  # -- *** [/home/install-package] *** --
+  INSTALL_DIR=/home/tiny-setup-package
+
+  # 先删除
+  rm -rf $INSTALL_DIR
+
+  # 临时文件记录进度数字
+  TEMP_FILE="/tmp/rsync_progress"
+  # 删除之前的进度文件
+  rm -rf $TEMP_FILE
+  # 创建新的临时文件
+  touch $TEMP_FILE
+  # 将 INSTALL_PACKAGE_DIR 拷贝至 /home/tiny-setup-package
+  rsync -a --info=progress2 --delete $INSTALL_PACKAGE_DIR $INSTALL_DIR >>$TEMP_FILE &
+
+  RSYNC_PID=$!
+
+  # 通过进程ID来判断rsync是否结束
+  while kill -0 $RSYNC_PID 2>/dev/null; do
+    if [[ -f "$TEMP_FILE" ]]; then
+      # 从临时文件中获取包含进度信息的那一行
+      PROGRESS_LINE=$(grep -oP '\d+%' "$TEMP_FILE" | tail -n 1)
+
+      if [[ -n "$PROGRESS_LINE" ]]; then
+        # 提取进度百分比
+        PROGRESS=$(echo "$PROGRESS_LINE" | grep -oP '\d+')
+        echo "$PROGRESS"
+      fi
+    fi
+    sleep 0.1
+  done
+
+  # 安装完成，删除临时文件
+  rm -rf "$TEMP_FILE"
+
+} | whiptail --title "复制文件" --gauge "正在复制,请稍等 ..." 6 60 0
+# --------------------------------------------------------------------
+
+
 # source
 source ./soft/script/common.sh
 
@@ -14,16 +61,9 @@ source ./soft/script/common.sh
 # 修改成相对路径，不一定要上传到 /opt 下
 [[ -d $INSTALL_DIR ]] && chmod -R 755 $INSTALL_DIR >/dev/null 2>&1
 
-# install package dir
-# 上传到 /opt/ 目录下面，然后安装的时候拷贝到 /home/ 下面
-# -- *** [/opt/tiny-setup-package/install-package] *** --
-INSTALL_PACKAGE_DIR=/opt/tiny-setup-package/install-package
-# 将 INSTALL_PACKAGE_DIR 拷贝至 /home/tiny-setup-package
-\cp -a $INSTALL_PACKAGE_DIR /home/tiny-setup-package
-
 # 重新创建日志文件
 DATE_STR=$(date "+%Y-%m-%d %H:%M:%S")
-echo "---> setup.sh [$DATE_STR] <---" > $SETUP_FILE
+echo "---> setup.sh [$DATE_STR] <---" >$SETUP_FILE
 
 # 2. 执行安装逻辑
 # 休眠时间
@@ -78,9 +118,9 @@ log_info "start setup..."
   done
 
   # 展示进度条
-} | whiptail --title "安装" --gauge "正在安装请稍等 ..." 6 60 0
+} | whiptail --title "安装" --gauge "正在安装,请稍等 ... \n详细日志:$SETUP_FILE" 8 60 0
 # 这边 --gauge 后面可以把安装信息拿出来展示一下
 
 # 提示安装完毕
-whiptail --title "安装" --msgbox "安装完毕,按下 enter 键继续." 10 60
+whiptail --title "安装" --msgbox "安装完毕,按下 enter 键继续 . \n详细日志:$SETUP_FILE" 10 60
 log_info "setup done."
