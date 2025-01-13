44b955376b42a0a0290174a5643688f9230ac861
xutao4 <xutao4@sungrowpower.com>
2025-01-13 12:13:49 +0800

    jenkins

--- tiny-setup/jenkins-setup/start_jenkins.sh
+++ tiny-setup/jenkins-setup/start_jenkins.sh
@@ -11,7 +11,19 @@ if [[ -f "jenkins.pid" ]]; then
   fi
 else
   echo "jenkins 未启动"
-  nohup /usr/local/java/bin/java -jar /opt/jenkins.war --httpPort=8081 --webroot=/opt/jenkins >/var/log/jenkins/jenkins.log 2>&1 &
+  nohup /usr/local/java/bin/java \
+    -Xms20480m \
+    -Xmx20480m \
+    -XX:MetaspaceSize=2560m \
+    -XX:MaxMetaspaceSize=5120m \
+    -Djava.awt.headless=true \
+    -Dhudson.lifecycle=hudson.lifecycleの中にJavaexecLifecycle \
+    -Djenkins.model.Jenkins.buildDiscarderLogSize=1000 \
+    -Djenkins.model.Jenkins.jobNameBlacklistRegex=.*forbidden.* \
+    -jar /opt/jenkins.war \
+    --httpPort=8081 \
+    --webroot=/opt/jenkins \
+    >/var/log/jenkins/jenkins.log 2>&1 &
   echo $! >jenkins.pid
   echo "jenkins 已启动"
 fi
